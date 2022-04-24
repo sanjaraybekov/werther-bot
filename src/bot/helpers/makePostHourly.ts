2a766b88cacb5aab0ebd38a63f71b2e62c65fabb
sanjaraybekov <sanjaraliaybekov@gmail.com>
2022-04-25 00:07:30 +0500

    aa

--- src/bot/helpers/makePostHourly.ts
+++ src/bot/helpers/makePostHourly.ts
@@ -1,5 +1,5 @@
 export const makePostHourly = (data: any, ctx: any) => {
   //   console.log(data);
 
-  return `Hourly`;
+  return `Hourly pressed`;
 };
--- src/bot/helpers/makePostWeekly.ts
+++ src/bot/helpers/makePostWeekly.ts
@@ -3,5 +3,5 @@ import { getDate } from "./garDate";
 export const makePostWeekly = (data: any, ctx: any) => {
   //   console.log(data);
 
-  return `Weekly`;
+  return `Weekly pressed`;
 };
